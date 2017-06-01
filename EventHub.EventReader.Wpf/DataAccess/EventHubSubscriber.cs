79f5e9029e25e80d3f597b28c5e45caed1097315
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-06-01 12:11:12 +0200

    Add initial event reader

---
+++ EventHub.EventReader.Wpf/DataAccess/EventHubSubscriber.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.IO;
+
+namespace ThomasClaudiusHuber.EventHub.Receiver.DataAccess
+{
+  public class EventHubSubscriber
+  {
+    private List<Task> _tasks;
+    private CancellationTokenSource _cts;
+    private CancellationToken _token;
+
+    public void SubscribeToEvents(string connectionString, int hoursAgoToStartFrom)
+    {
+      if (hoursAgoToStartFrom > 0)
+      {
+        throw new ArgumentException("Should contain zero or a negative value", nameof(hoursAgoToStartFrom));
+      }
+      if (_cts != null)
+      {
+        _cts.Cancel();
+      }
+
+      var client = EventHubClient.CreateFromConnectionString(connectionString);
+
+      EventHubConsumerGroup consumerGroup = client.GetDefaultConsumerGroup();
+      string[] partitionIds = client.GetRuntimeInformation().PartitionIds;
+
+      List<EventHubReceiver> receivers =
+      partitionIds.Select(
+        partitionId => consumerGroup.CreateReceiver(partitionId, DateTime.UtcNow.AddHours(hoursAgoToStartFrom))).ToList();
+
+      _cts = new CancellationTokenSource();
+      _token = _cts.Token;
+
+      _tasks = new List<Task>();
+      foreach (var receiver in receivers)
+      {
+        var task = Task.Run(() =>
+        {
+          while (true)
+          {
+            try
+            {
+              if (_token.IsCancellationRequested)
+              {
+                break;
+              }
+
+              var message = receiver.Receive();
+
+              if (message != null)
+              {
+                string body = Encoding.UTF8.GetString(message.GetBytes());
+                OnEventReceived(body);
+              }
+            }
+            catch (Exception ex)
+            {
+              Debug.WriteLine(ex.Message);
+            }
+          }
+
+        }, _token);
+        _tasks.Add(task);
+      }
+    }
+
+    public event EventHandler<string> EventReceived;
+
+    protected virtual void OnEventReceived(string data)
+    {
+      EventReceived?.Invoke(this, data);
+    }
+  }
+}
---
+++ EventHub.EventReader.Wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,105 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using ThomasClaudiusHuber.EventHub.Receiver.DataAccess;
+
+namespace ThomasClaudiusHuber.EventHub.Receiver.ViewModel
+{
+  public class MainViewModel : ViewModelBase
+  {
+    private string _receivedEvents="";
+    private bool _isStarting;
+    private int _selectedHoursAgoToStartFrom;
+    private string _connectionString = "";
+    private readonly EventHubSubscriber _eventHubSubscriber;
+    private static readonly object _lock = new object();
+    public MainViewModel(EventHubSubscriber eventHubSubscriber)
+    {
+      _eventHubSubscriber = eventHubSubscriber;
+      _eventHubSubscriber.EventReceived += EventHubAccess_SensorDataReceived;
+
+      StartReceivingEventsCommand = new DelegateCommand(OnStartReceivingEventsExecute, OnStartReceivingEventsCanExecute);
+
+      var hoursAgoToStartFrom = new List<int>();
+      for (int i = 0; i >= -24; i--)
+      {
+        hoursAgoToStartFrom.Add(i);
+      }
+      HoursAgoToStartFromList = hoursAgoToStartFrom;
+    }
+
+    public string ReceivedEvents
+    {
+      get { return _receivedEvents; }
+      set
+      {
+        _receivedEvents = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private IEnumerable<int> _hoursAgoToStartFromList;
+
+    public IEnumerable<int> HoursAgoToStartFromList
+    {
+      get { return _hoursAgoToStartFromList; }
+      set { _hoursAgoToStartFromList = value; }
+    }
+
+    public ICommand StartReceivingEventsCommand { get; }
+
+    public int SelectedHoursAgoToStartFrom
+    {
+      get { return _selectedHoursAgoToStartFrom; }
+      set
+      {
+        _selectedHoursAgoToStartFrom = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string ConnectionString
+    {
+      get { return _connectionString; }
+      set
+      {
+        _connectionString = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private bool OnStartReceivingEventsCanExecute()
+    {
+      return !_isStarting;
+    }
+
+    private void OnStartReceivingEventsExecute()
+    {
+      _isStarting = true;
+      ((DelegateCommand)StartReceivingEventsCommand).RaiseCanExecuteChanged();
+      try
+      {
+        _eventHubSubscriber.SubscribeToEvents(ConnectionString, SelectedHoursAgoToStartFrom);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        _isStarting = false;
+        ((DelegateCommand)StartReceivingEventsCommand).RaiseCanExecuteChanged();
+      }
+    }
+
+    private void EventHubAccess_SensorDataReceived(object sender, string data)
+    {
+      lock (_lock)
+      {
+        ReceivedEvents = ReceivedEvents.Insert(0, data + Environment.NewLine);
+      }
+    }
+  }
+}
